0a24815b4418e5e08ef19e4e920e3ed79fa6cf04
pedro.mendes <pedro.mendes@corp.globo.com>
2018-05-16 12:01:42 -0300

    [WIP] main: move lat/long references to a struct called Coordinate

--- main.go
+++ main.go
@@ -2,8 +2,7 @@ package distance_calculator
 
 import "math"
 
-const(
-
+const (
 	// Unit in Meter
 	UnitMeter = "METER"
 
@@ -17,6 +16,11 @@ const(
 	UnitNautical = "NAUTICAL"
 )
 
+type Coordinate struct {
+	Latitude  float64
+	Longitude float64
+}
+
 // deg2rad converts decimal degrees to radians
 func deg2rad(deg float64) float64 {
 	return deg * math.Pi / 180.0
@@ -27,17 +31,17 @@ func rad2deg(rad float64) float64 {
 	return (rad * 180.0) / math.Pi
 }
 
-// calculate
-func Calculate(firstLatitude float64, firstLongitude float64, secondLongitude float64, secondLatitude float64, unit string) (dist float64) {
+// calculate two coordinates and return the distance according to a given dist unit
+func Calculate(first Coordinate, second Coordinate, unit string) (dist float64) {
 
-	theta := firstLongitude - secondLongitude
-	dist = math.Sin((deg2rad(firstLatitude)) * math.Sin(deg2rad(secondLatitude)) + math.Cos(deg2rad(firstLatitude)) * math.Cos(deg2rad(secondLatitude)) * math.Cos(deg2rad(theta)))
+	theta := first.Longitude - second.Longitude
+	dist = math.Sin((deg2rad(first.Latitude))*math.Sin(deg2rad(second.Latitude)) + math.Cos(deg2rad(first.Latitude))*math.Cos(deg2rad(second.Latitude))*math.Cos(deg2rad(theta)))
 	dist = math.Acos(dist)
 	dist = rad2deg(dist)
 	dist = dist * 60
 
 	if unit == UnitMile {
-		dist = dist *  1.1515
+		dist = dist * 1.1515
 	} else if unit == UnitMeter {
 		dist = dist * 0.609344
 	} else if unit == UnitKilometer {
@@ -48,21 +52,32 @@ func Calculate(firstLatitude float64, firstLongitude float64, secondLongitude fl
 		dist = -1
 	}
 	return
-
 }
 
+// CalcMeters calculates the distance between two coordinates in Meters
 func CalcMeters(firstLatitude float64, firstLongitude float64, secondLongitude float64, secondLatitude float64) {
-	Calculate(firstLatitude, firstLongitude, secondLongitude, secondLatitude, UnitMeter)
+	first := Coordinate{Latitude: firstLatitude, Longitude: firstLongitude}
+	second := Coordinate{Latitude: secondLatitude, Longitude: secondLongitude}
+	Calculate(first, second, UnitMeter)
 }
 
+// CalcMiles calculates the distance between two coordinates in Miles
 func CalcMiles(firstLatitude float64, firstLongitude float64, secondLongitude float64, secondLatitude float64) {
-	Calculate(firstLatitude, firstLongitude, secondLongitude, secondLatitude, UnitMile)
+	first := Coordinate{Latitude: firstLatitude, Longitude: firstLongitude}
+	second := Coordinate{Latitude: secondLatitude, Longitude: secondLongitude}
+	Calculate(first, second, UnitMile)
 }
 
+// CalcKilometers calculates the distance between two coordinates in Kilometers
 func CalcKilometers(firstLatitude float64, firstLongitude float64, secondLongitude float64, secondLatitude float64) {
-	Calculate(firstLatitude, firstLongitude, secondLongitude, secondLatitude, UnitKilometer)
+	first := Coordinate{Latitude: firstLatitude, Longitude: firstLongitude}
+	second := Coordinate{Latitude: secondLatitude, Longitude: secondLongitude}
+	Calculate(first, second, UnitKilometer)
 }
 
-func CalcNauticals(firstLatitude float64, firstLongitude float64, secondLongitude float64, secondLatitude float64) {
-	Calculate(firstLatitude, firstLongitude, secondLongitude, secondLatitude, UnitNautical)
+// CalcNautical calculates the distance between two coordinates in Nautical
+func CalcNautical(firstLatitude float64, firstLongitude float64, secondLongitude float64, secondLatitude float64) {
+	first := Coordinate{Latitude: firstLatitude, Longitude: firstLongitude}
+	second := Coordinate{Latitude: secondLatitude, Longitude: secondLongitude}
+	Calculate(first, second, UnitNautical)
 }
