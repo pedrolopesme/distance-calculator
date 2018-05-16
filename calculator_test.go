8d40608881ea876ed8623103c6f3f5bd219b02da
pedro.mendes <pedro.mendes@corp.globo.com>
2018-05-16 15:01:11 -0300

    calculator_test: add tests to CalMeter func

--- calculator_test.go
+++ calculator_test.go
@@ -5,6 +5,21 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func TestCalcMetersWithNoDistanceBetweenTwoPoints(test *testing.T) {
+	dist := CalcMeters(0.0,0.0,0.0,0.0)
+	assert.Equal(test, 0.0, dist)
+}
+
+func TestCalcMetersWithVerySmallDistanceBetweenTwoPoints(test *testing.T) {
+	dist := CalcMeters(0.00001,0.0,0.0,0.0)
+	assert.Equal(test,  1.1046843560467916e-06, dist)
+}
+
+func TestCalcMetersWithVeryLongDistanceBetweenTwoPoints(test *testing.T) {
+	dist := CalcMeters(90.0,90.0,0.0,0.0)
+	assert.Equal(test, 10.007037053999998, dist)
+}
+
 func TestCalcKilometersWithNoDistanceBetweenTwoPoints(test *testing.T) {
 	dist := CalcKilometers(0.0,0.0,0.0,0.0)
 	assert.Equal(test, 0.0, dist)
