7f9bbb7d5cc659dac58ead81b8d7c842ce9db170
pedro.mendes <pedro.mendes@corp.globo.com>
2018-05-16 14:55:16 -0300

    calculator_test: add tests to Kilometers

--- calculator_test.go
+++ calculator_test.go
@@ -6,6 +6,16 @@ import (
 )
 
 func TestCalcKilometersWithNoDistanceBetweenTwoPoints(test *testing.T) {
-	dist := CalcKilometers(1.0,2.0,1.0,2.0)
-	assert.Equal(test, 0, dist)
+	dist := CalcKilometers(0.0,0.0,0.0,0.0)
+	assert.Equal(test, 0.0, dist)
+}
+
+func TestCalcKilometersWithVerySmallDistanceBetweenTwoPoints(test *testing.T) {
+	dist := CalcKilometers(0.00001,0.0,0.0,0.0)
+	assert.Equal(test, 0.0011111329509752147, dist)
+}
+
+func TestCalcKilometersWithVeryLongDistanceBetweenTwoPoints(test *testing.T) {
+	dist := CalcKilometers(90.0,90.0,0.0,0.0)
+	assert.Equal(test, 10007.037053999999, dist)
 }
